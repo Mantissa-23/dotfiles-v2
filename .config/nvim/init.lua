ff909a46df2f1e23f551fb89d61efb7657d91cdc
Dylan Culfogienis <dylan@studio-mantissa.com>
2024-07-09 20:55:00 -0600

    Terminal neovim is in a good state

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,31 +15,125 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Environment-dependent mappings
+-- Environment-dependent config
 if vim.g.vscode then
-  -- Make sure to setup `mapleader` and `maplocalleader` before
-  -- loading lazy.nvim so that mappings are correct.
-  -- This is also a good place to setup other settings (vim.opt)
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = "\\"
+
 else
+  -- Line numbers please
+  vim.wo.number = true
+
+  -- two semicolons to escape
   vim.keymap.set('i', '<;;>', '<Esc', { desc = 'Composite escape with ;;' })
 
+  -- Natural split navigation
   vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
   vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
   vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
   vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
 end
 
+-- Both!
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Disable [Enter to continue] messages
+
+-- Do not exit visual mode when indenting
+vim.keymap.set('v', '<', '<gv', { noremap = true})
+vim.keymap.set('v', '>', '>gv', { noremap = true})
 
 -- Init lazyvim
 require("lazy").setup({
   spec = {
-    {
+    -- Non-vscodium plugins- I.e. the "IDE" stuff
+    { -- My favorite colorscheme :)
       "luisiacc/gruvbox-baby",
       cond = not vim.g.vscode,
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd([[colorscheme gruvbox-baby]])
+      end,
     },
-    {
+    { -- For syntax highlighting
+      "nvim-treesitter/nvim-treesitter",
+      cond = not vim.g.vscode,
+      build = ":TSUpdate",
+      config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+          ensure_installed = { "lua", "vim", "vimdoc", "markdown" },
+          auto_install = true,
+        })
+      end,
+    },
+    { -- If no editorconfig is present, guess the indentation of a file when we open it
+      "nmac427/guess-indent.nvim",
+      cond = not vim.g.vscode,
+      event = "BufRead",
+      config = function() require('guess-indent').setup {} end,
+    },
+    { -- Add lines to denote indent depth
+      "lukas-reineke/indent-blankline.nvim",
+      cond = not vim.g.vscode,
+      main = "ibl",
+      opts = {
+        indent = {
+          char = "▏",
+        },
+        scope = {
+          char = "▍"
+        },
+      },
+    },
+    { -- File explorer
+      "nvim-tree/nvim-tree.lua",
+      cond = not vim.g.vscode,
+      version = "*",
+      lazy = false,
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      keys = {
+        { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle File Explorer" },
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end,
+    },
+    { -- Fuzzy finder
+      'nvim-telescope/telescope.nvim',
+      cond = not vim.g.vscode,
+      branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      keys = {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+        { "<leader>fs", "<cmd>Telescope find_files<cr>", desc = "Search (ripgrep) everything" },
+      },
+    },
+    { -- Which Key
+      "folke/which-key.nvim",
+      cond = not vim.g.vscode,
+      event = "VeryLazy",
+      init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 100
+
+        -- Just name categories here; assign keys elsewhere
+        require("which-key").register({
+          f = { name = "Find" }
+        }, { prefix = "<leader>" })
+      end,
+      opts = {
+      }
+    },
+    -- "All" plugins- QoL and editor shortcut stuff
+    { -- cs, vs, ds, ys all 
       "kylechui/nvim-surround",
       version = "*",
       event = "VeryLazy",
@@ -49,16 +143,15 @@ require("lazy").setup({
         })
       end,
     },
-
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = {
-      colorscheme = {
-        "gruvbox-baby",
-        cond = not vim.g.vscode,
-      }
-  },
+  -- install = {
+  --   colorscheme = {
+  --     "gruvbox-baby",
+  --     cond = not vim.g.vscode,
+  --   }
+  -- },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
