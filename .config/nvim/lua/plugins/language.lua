d7d4d3a20277a593c5c96ce4c243823f35e272ba
Dylan Culfogienis <dylan@studio-mantissa.com>
2025-02-20 17:21:29 -0500

    Fucking switched completion plugins for the third time to nvim-cmp

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -22,50 +22,8 @@ return {
     cond = not vim.g.vscode,
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
-      { "ms-jpq/coq_nvim", branch = "coq" },
-      { "ms-jpq/coq.artifacts", branch = "artifacts" },
-      { 'ms-jpq/coq.thirdparty', branch = "3p" },
     },
-    init = function()
-      vim.g.coq_settings = {
-        auto_start = true,
-        keymap = {
-          jump_to_mark = "<c-n>"
-        }
-      }
-    end,
     config = function()
-      local lspconfig = require('lspconfig')
-
-      lspconfig.csharp_ls.setup {
-        autostart = true,
-      }
-
-      lspconfig.vtsls.setup {
-        autostart = true,
-        settings = {
-          vtsls = {
-            experimental = {
-              completion = {
-                enableServerSideFuzzyMatch = true,
-                enableProjectDiagnostics = true,
-              },
-            },
-          },
-        }
-      }
-
-      lspconfig.lua_ls.setup {
-        autostart = true,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
-            }
-          }
-        }
-      }
-
       -- Unbind default bindings
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(ev)
@@ -94,6 +52,110 @@ return {
       { "<C-.>", function() vim.lsp.buf.code_action() end, desc = "Code Actions"},
       { "<leader>lD", function() vim.diagnostic.open_float() end, desc = "LSP Diagnostics (on cursor)" },
     },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+    },
+    config = function()
+      local cmp = require('cmp');
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            local insert = require('mini.snippets').config.expand.insert or require('mini.snippets').default_insert
+            insert({ body = args.body }) -- Insert at cursor
+            cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+            require("cmp.config").set_onetime({ sources = {} })
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+      -- Set configuration for specific filetype.
+      --[[ cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'git' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+      require("cmp_git").setup() ]]-- 
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+
+      -- Set up lspconfig.
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      require('lspconfig')['lua_ls'].setup {
+        capabilities = capabilities,
+        autostart = true,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
+      }
+      require('lspconfig')['csharp_ls'].setup {
+        autostart = true,
+        capabilities = capabilities,
+      }
+      require('lspconfig')['vtsls'].setup {
+        capabilities = capabilities,
+        autostart = true,
+        settings = {
+          vtsls = {
+            experimental = {
+              completion = {
+                enableServerSideFuzzyMatch = true,
+                enableProjectDiagnostics = true,
+              },
+            },
+          },
+        }
+      }
+    end,
   },
   {
     "echasnovski/mini.snippets",
